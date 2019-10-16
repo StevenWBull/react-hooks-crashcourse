450f3a13e4823974a2ec436a09698dd288494d75
Steven Bull <stevenwbulljr@gmail.com>
2019-10-16 16:14:31 +0000

    finish form custom hook

--- src/useForm.js
+++ src/useForm.js
@@ -1,5 +1,15 @@
 import { useState } from 'react';
 
 export const useForm = (initialValues) => {
-  const { email, setEmail } = useState(initialValues);
+  const { values, setValues } = useState(initialValues);
+
+
+  //can return an object or array
+  return [ values, e => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value
+    })
+  }
+  ]
 }
